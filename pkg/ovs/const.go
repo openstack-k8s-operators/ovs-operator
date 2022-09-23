7805279c2b89bb33b866ff5bb780e93c9d1f651e
Slawek Kaplonski <skaplons@redhat.com>
2022-09-23 12:01:53 +0200

    Add ovs package

---
+++ pkg/ovs/const.go
@@ -0,0 +1,14 @@
+package ovs
+
+const (
+	// Ovs vswitchd ServiceName -
+	ServiceName = "ovs"
+	// ovn-controller ServiceName
+	OvnControllerServiceName = "ovn-controller"
+	// ServiceType -
+	ServiceType = "ovs"
+	// ServiceAccountName -
+	ServiceAccountName = "ovs-operator-ovs"
+	// KollaConfigAPI
+	KollaConfigAPI = "/var/lib/config-data/merged/ovs-config.json"
+)
