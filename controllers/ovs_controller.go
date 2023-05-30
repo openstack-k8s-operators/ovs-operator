11aa79b3f4e2685ca959f91d351c12c3764d3274
Takashi Kajinami <tkajinam@redhat.com>
2023-05-30 14:18:20 +0900

    Remove unused conditions

--- controllers/ovs_controller.go
+++ controllers/ovs_controller.go
@@ -145,8 +145,6 @@ func (r *OVSReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result
 		instance.Status.Conditions = condition.Conditions{}
 		// initialize conditions used later as Status=Unknown
 		cl := condition.CreateList(
-			condition.UnknownCondition(condition.DBReadyCondition, condition.InitReason, condition.DBReadyInitMessage),
-			condition.UnknownCondition(condition.ExposeServiceReadyCondition, condition.InitReason, condition.ExposeServiceReadyInitMessage),
 			condition.UnknownCondition(condition.InputReadyCondition, condition.InitReason, condition.InputReadyInitMessage),
 			condition.UnknownCondition(condition.ServiceConfigReadyCondition, condition.InitReason, condition.ServiceConfigReadyInitMessage),
 			condition.UnknownCondition(condition.DeploymentReadyCondition, condition.InitReason, condition.DeploymentReadyInitMessage),
